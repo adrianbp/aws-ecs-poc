c266d0539e6733d80fb6358ef64e2fcba2a27213
adrianbp <adribenig@hotmail.com>
2025-10-24 17:48:27 -0300

    testando

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -22,19 +22,3 @@ output "load_balancer_dns" {
   description = "Endpoint HTTP público do ALB"
   value       = aws_lb.this.dns_name
 }
-
-output "github_actions_role_arn" {
-  description = "ARN da role IAM usada pelo GitHub Actions para deploy"
-  value       = aws_iam_role.github_actions.arn
-}
-
-output "datadog_secret_name" {
-  description = "Nome do secret do Datadog usado pelo sidecar"
-  value       = var.enable_datadog_agent ? try(aws_secretsmanager_secret.datadog[0].name, var.datadog_api_key_secret_arn) : null
-}
-
-output "datadog_secret_arn" {
-  description = "ARN do secret do Datadog usado pelo sidecar"
-  value       = var.enable_datadog_agent ? try(aws_secretsmanager_secret.datadog[0].arn, var.datadog_api_key_secret_arn) : null
-  sensitive   = true
-}
