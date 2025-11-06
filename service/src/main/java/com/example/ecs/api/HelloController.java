e38c7aec50177e574fe791c608bbee54dc0069d3
adrianbp <adribenig@hotmail.com>
2025-11-06 20:30:09 -0300

    subindo ajuste

--- service/src/main/java/com/example/ecs/api/HelloController.java
+++ service/src/main/java/com/example/ecs/api/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({"/api","/dynatrace/api"})
+@RequestMapping({"/api","/dynatrace/api","/native/api"})
 public class HelloController {
 
     @Value("${spring.application.name}")
