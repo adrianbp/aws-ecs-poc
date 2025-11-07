11f4ca48d7d86981686c56ad849446b259679a6b
adrianbp <adribenig@hotmail.com>
2025-11-07 09:00:07 -0300

    subifa aplicacao

--- service/src/main/java/com/example/ecs/EcsDemoApplication.java
+++ service/src/main/java/com/example/ecs/EcsDemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.ecs;
 
+import com.example.ecs.api.GreetingRecord;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RegisterReflectionForBinding(GreetingRecord.class)
 public class EcsDemoApplication {
 
     public static void main(String[] args) {
