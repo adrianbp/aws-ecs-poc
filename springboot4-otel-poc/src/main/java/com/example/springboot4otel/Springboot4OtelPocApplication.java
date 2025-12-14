d0611bafc4c1e5056968af5b64e89485988d05f6
adrianbp <adribenig@hotmail.com>
2025-12-14 13:09:50 -0300

    Debug: Force java version properties to 25 exactly

--- springboot4-otel-poc/src/main/java/com/example/springboot4otel/Springboot4OtelPocApplication.java
+++ springboot4-otel-poc/src/main/java/com/example/springboot4otel/Springboot4OtelPocApplication.java
@@ -11,20 +11,31 @@ import org.springframework.context.annotation.Bean;
 public class Springboot4OtelPocApplication {
 
     public static void main(String[] args) {
-        System.out.println("DEBUG: Dumping version properties:");
+        System.out.println("DEBUG: Dumping version properties BEFORE override:");
         System.out.println("java.version: " + System.getProperty("java.version"));
         System.out.println("java.vm.version: " + System.getProperty("java.vm.version"));
         System.out.println("java.specification.version: " + System.getProperty("java.specification.version"));
         System.out.println("java.vm.specification.version: " + System.getProperty("java.vm.specification.version"));
         System.out.println("java.vendor.version: " + System.getProperty("java.vendor.version"));
         
-        // Force all to 25 to try and satisfy the check
+        // Force all to "25" exactly to try and satisfy the check
         System.setProperty("java.version", "25");
         System.setProperty("java.vm.version", "25");
         System.setProperty("java.specification.version", "25");
         System.setProperty("java.vm.specification.version", "25");
+        System.setProperty("java.vendor.version", "Oracle GraalVM 25");
+
+        System.out.println("DEBUG: Properties AFTER override:");
+        System.out.println("java.version: " + System.getProperty("java.version"));
+        System.out.println("java.specification.version: " + System.getProperty("java.specification.version"));
         
-        SpringApplication.run(Springboot4OtelPocApplication.class, args);
+        try {
+            SpringApplication.run(Springboot4OtelPocApplication.class, args);
+        } catch (Exception e) {
+            System.out.println("DEBUG: Caught exception in main:");
+            e.printStackTrace();
+            throw e;
+        }
     }
 
     @Bean
